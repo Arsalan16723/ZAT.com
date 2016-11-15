a1861dcbc723d5b349d4bed06c706f226089b22b
Arsalan <m.arsalan16723@gmail.com>
2016-11-15 21:48:03 +0000

    Added ctegories show edit update and delete actions

--- app/controllers/pcategories_controller.rb
+++ app/controllers/pcategories_controller.rb
@@ -1,4 +1,5 @@
 class PcategoriesController < ApplicationController
+  before_action :find_category, only: [:show, :edit, :update, :destroy]
   def new
     @pcategory = Pcategory.new
   end
@@ -13,19 +14,33 @@ class PcategoriesController < ApplicationController
   end
 
   def index
-    @pcategories = Pcategory.all
+    @pcategories = Pcategory.all 
   end
-
+  def show
+    
+  end
   def edit
   end
-
   def update
+    if @pcategoryt.update pcategory_params
+      redirect_to @pcategory, notice: 'Updated'
+    else
+      render 'edit', notice: 'Error!'
+    end
   end
 
   def destroy
+    if @pcategory.destroy
+      redirect_to pcategories_path, notice: 'Deleted'
+    else
+      redirect_to @pcategory, notice: 'Error to deleted'
+    end
   end
   private
   def pcategory_params
     params.require(:pcategory).permit(:name)
+  end
+  def find_category
+    @pcategory = Pcategory.find(params[:id])
   end
 end
